29f0ae9c829c7036271802645de74ffa77246596
Andy Pickler <andy.pickler@gmail.com>
2016-10-20 05:56:49 -0600

    Add routing and styles Ready for https://angular.io/docs/ts/latest/tutorial/toh-pt6.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
-
-import { HeroService } from './hero.service';
-import { Hero } from "./hero";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tour of Heroes';
-  selectedHero: Hero;
-  heroes: Hero[];
-
-  constructor(private heroService: HeroService) {}
-
-  ngOnInit(): void {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
